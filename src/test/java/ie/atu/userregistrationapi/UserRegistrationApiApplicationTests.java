4d761c8f660793003633d21fa0a6fc4c3d2989f0
G00410024@atu.ie <G00410024@atu.ie>
2023-10-04 14:24:52 +0100

    Project initialised

---
+++ src/test/java/ie/atu/userregistrationapi/UserRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.userregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
